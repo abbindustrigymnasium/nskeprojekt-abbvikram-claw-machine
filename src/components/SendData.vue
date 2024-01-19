ce0716250858a5aa35c8585430688d787f2122e8
viktor723 <viktor.ramic.simic@abbgymnasiet.se>
2024-01-19 23:11:25 +0100

    slider + andra functions

--- src/components/SendData.vue
+++ src/components/SendData.vue
@@ -1,13 +1,20 @@
 <template>
     <div>
-      <form @submit.prevent="submitData">
-        <label for="dataInput">Data:</label>
-        <input v-model="data" type="text" id="dataInput" />
-        <button type="submit">Submit</button>
-      </form>
+      <label for="dataInput">Data:</label>
+      <input v-model="data" type="text" id="dataInput" />
   
-      <!-- Button to send the number 1 -->
-      <button @click="sendNumber">Send Number 1</button>
+      <!-- Slider -->
+      <label for="slider">Slider Value: {{ slider }}</label>
+      <input id="slider" type="range" v-model="slider" min="0" max="180" @input="updateSlider">
+  
+      <!-- Button to continuously add 1 for every 5 milliseconds -->
+      <button @mousedown="startAdding" @mouseup="stopAdding">Hold to Add 1</button>
+  
+      <!-- Button to continuously subtract 1 for every 5 milliseconds -->
+      <button @mousedown="startSubtracting" @mouseup="stopSubtracting">Hold to Subtract 1</button>
+  
+      <!-- Display the counter value -->
+      <p>Counter: {{ counter }}</p>
     </div>
   </template>
   
@@ -18,22 +25,53 @@
     data() {
       return {
         data: '',
+        slider: 0,
+        addingInterval: null,
+        subtractingInterval: null,
+        counter: 0,
       };
     },
     methods: {
-      submitData() {
-        // Replace everything at the specified path 'your_data_path'
+      startAdding() {
+        // Start adding 1 for every 5 milliseconds when the button is pressed
+        this.addingInterval = setInterval(() => {
+          this.counter = Math.min(180, this.counter + 1);
+  
+          set(ref(db, 'servo'), {
+            data: this.counter,
+            slider: parseInt(this.slider),
+          });
+        }, 5); // 5 milliseconds interval
+      },
+      stopAdding() {
+        // Stop adding when the button is released
+        clearInterval(this.addingInterval);
+        this.addingInterval = null;
+      },
+      startSubtracting() {
+        // Start subtracting 1 for every 5 milliseconds when the button is pressed
+        this.subtractingInterval = setInterval(() => {
+          this.counter = Math.max(0, this.counter - 1);
+  
+          set(ref(db, 'servo'), {
+            data: this.counter,
+            slider: parseInt(this.slider),
+          });
+        }, 5); // 5 milliseconds interval
+      },
+      stopSubtracting() {
+        // Stop subtracting when the button is released
+        clearInterval(this.subtractingInterval);
+        this.subtractingInterval = null;
+      },
+      updateSlider() {
+        // Handle slider value changes
+        console.log(`Updated Slider Value: ${this.slider}`);
+  
+        // Send the slider data to Firebase immediately
         set(ref(db, 'servo'), {
           data: this.data,
-        });
-  
-        // Clear the input field after submission
-        this.data = '';
-      },
-      sendNumber() {
-        // Replace everything at the specified path 'your_data_path'
-        set(ref(db, 'motor'), {
-          data: 1,
+          slider: parseInt(this.slider),
         });
       },
     },
